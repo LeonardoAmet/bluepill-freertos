42bcd56eb7fec3ad816c5c7417ae2f477d4a3ae6
Leonardo Amet <leonardo.amet@gmail.com>
2025-04-30 21:00:56 +0000

    Lab 01: Blink funcional usando libopencm3 + linker y makefile configurados

--- common/startup.s
+++
@@ -1,9 +0,0 @@
-.syntax unified
-.cpu cortex-m3
-.thumb
-
-.global reset_handler
-.type reset_handler, %function
-
-reset_handler:
-    b main
--- common/system_stm32f1xx.c
+++
@@ -1,4 +0,0 @@
-// Stub de system_stm32f1xx.c
-void SystemInit(void) {
-    // Dejar vacío o personalizado si se desea
-}
